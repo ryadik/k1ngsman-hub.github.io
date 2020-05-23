3caaecc20a2d9594fb14a08c2b7944521ff47a00
K1nGsmaN-hub <15162342h@gmail.com>
2020-05-23 20:13:07 +0800

    luxestate_v0.8

--- script/main.js
+++ script/main.js
@@ -1,8 +1,27 @@
-const menuButton = document.querySelector('#menu__burger');
-const menuLinksContainer = document.querySelector('#menu__links');
+//burger menu
+const menuButton = document.querySelector('#menu__burger'); // get burgegBtn
+const menuLinksContainer = document.querySelector('#menu__links'); // get div.menu__links
+const bodyTag = document.querySelector('body');
 
 menuButton.onclick = function(){
-    menuLinksContainer.classList.toggle('active');
-    menuButton.classList.toggle('active');
+    menuLinksContainer.classList.toggle('active'); // take active class to div.menu__links
+    menuButton.classList.toggle('active'); // take active class to burgerBtn
+    bodyTag.classList.toggle('active'); // take active class to body
 }
 
+
+// smooth scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
